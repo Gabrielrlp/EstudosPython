2c838219ca1277b8ea4ff2782a6ebb174879327f
gabs0808 <bielzinxpwar@gmail.com>
2026-09-11 23:07:09 -0300

    add

---
+++ ex02.py
@@ -0,0 +1,31 @@
+# nome = str(input('Digite seu nome: '))
+# idade = int(input('Digite sua idade: '))
+# altura = float(input('Digite sua altura: '))
+# matricula = int(input('Digite sua matricula: '))
+# curso = str(input('Digite seu curso: '))
+# disciplina = str(input('Digite sua disciplina: '))
+# nota = float(input('Digite sua nota: '))
+
+# print(
+#     f'\nNome: {nome}\n'
+#     f'Idade: {idade}\n'
+#     f'Altura: {altura}\n'
+#     f'Matricula: {matricula}\n'
+#     f'Curso: {curso}\n'
+#     f'Disciplina: {disciplina}'
+# )
+
+# if nota >= 6 and nota <= 10:
+#     print('Status: Aprovado')
+# elif nota <= 5 and nota >= 0:
+#     print('Status: Reprovado')
+# else:
+#     print('Status: Nota invalida')
+
+# for i in range(10):
+#     produto = int(input('nome do produto: '))
+
+# for i in range(10):
+#     print(produto)
+#     produto = produto + 1
+# print(produto)
